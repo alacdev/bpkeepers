86c51ef6e69721c93f411c23020b57544944c7fe
Hugo Alén Acuña <hugoaalen@gmail.com>
2024-05-08 14:22:30 +0200

    primeros pasos

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.getElementById('animateButton').addEventListener('click', function() {
-    var content = document.querySelector('.content');
-    content.classList.toggle('hidden');
-  });
-  
+/* Animación botones al hacer scroll */
+window.addEventListener('scroll', function() {
+  var entrenamientos = document.querySelector('.entrenamientos');
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 400) { // Cambia este valor según la posición en la que desees que aparezcan los botones
+    entrenamientos.classList.add('show');
+  } else {
+    entrenamientos.classList.remove('show');
+  }
+});

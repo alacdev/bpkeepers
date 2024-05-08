00f6eb13d8763a47a48fa36a799953690675adc9
Hugo Alén Acuña <hugoaalen@gmail.com>
2024-05-08 12:39:54 +0200

    Base de la web

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('animateButton').addEventListener('click', function() {
+    var content = document.querySelector('.content');
+    content.classList.toggle('hidden');
+  });
+  

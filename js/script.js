35a5e89ab5f327d5d3390994b0ae0ecc2d3ce9a3
Hugo Alén Acuña <hugoaalen@gmail.com>
2024-05-13 12:33:46 +0200

    j

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-/* Animación botones al hacer scroll */
 window.addEventListener('scroll', function() {
   var entrenamientos = document.querySelector('.entrenamientos');
   var scrollPosition = window.scrollY;
 
-  if (scrollPosition > 400) { // Cambia este valor según la posición en la que desees que aparezcan los botones
-    entrenamientos.classList.add('show');
+  if (scrollPosition > 400) { 
   } else {
     entrenamientos.classList.remove('show');
   }

aaed9dceedcf036935ebe6ffd40e0418bc61fcdf
Hugo Alén Acuña <hugoaalen@gmail.com>
2024-05-13 12:48:30 +0200

    js integrado y php out

--- js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-  var entrenamientos = document.querySelector('.entrenamientos');
-  var scrollPosition = window.scrollY;
-
-  if (scrollPosition > 400) { 
-  } else {
-    entrenamientos.classList.remove('show');
-  }
-});
